425885355a3135b23d84918fbc0a47b696841d7f
lgambier <lgambier@agencedps.com>
2022-06-02 12:01:55 +0200

    fix: post error

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -218,7 +218,7 @@ export default {
         method: "POST",
         headers: new Headers({
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Origin": "*",
         }),
         body: JSON.stringify({
           email: "test@test-davril.getunlatch.com",
